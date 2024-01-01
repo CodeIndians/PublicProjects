a008736f9b3ef678bde60be8cc3ffcb14941b7dc
satishchandra.d <satishchandra.d@celestialsys.com>
2024-01-01 16:48:57 +0530

    Project to demontrate the use of Stateless Nuget Pacakge in AOT compile console application

---
+++ AOTConsoleAppWithStatelessPackage/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Stateless;
+
+class Program
+{
+    enum State { Green, Yellow, Red }
+
+    static void Main()
+    {
+        var machine = new StateMachine<State, string>(State.Green);
+
+        machine.Configure(State.Green)
+            .Permit("Change", State.Yellow);
+
+        machine.Configure(State.Yellow)
+            .Permit("Change", State.Red);
+
+        machine.Configure(State.Red)
+            .Permit("Change", State.Green);
+
+        Console.WriteLine($"Current state: {machine.State}");
+
+        Console.Write("Press 'Enter' to transition to the next state...");
+        Console.ReadLine();
+
+        machine.Fire("Change");
+
+        Console.WriteLine($"Current state: {machine.State}");
+
+        Console.Write("Press any key to close the application");
+        Console.ReadLine();
+    }
+}
