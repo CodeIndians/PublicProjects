83d1fd15801a7ce160d7d45487f9da86ea5de4e6
satishchandra.d <satishchandra.d@celestialsys.com>
2024-01-02 14:06:30 +0530

    Adding SQLite Package and testing with help of a driver program

--- AOTConsoleAppWithStatelessPackage/Program.cs
+++ AOTConsoleAppWithStatelessPackage/Program.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using Stateless;
+using System.Data.SQLite;
 
 class Program
 {
     enum State { Green, Yellow, Red }
 
     static void Main()
+    {
+        TestStateless();
+        TestSQLite();
+    }
+
+    static void TestStateless()
     {
         var machine = new StateMachine<State, string>(State.Green);
 
@@ -30,4 +37,39 @@ class Program
         Console.Write("Press any key to close the application");
         Console.ReadLine();
     }
+
+    static void TestSQLite()
+    {
+        Console.WriteLine("SQLite AOT Console App");
+
+        using (var connection = new SQLiteConnection("Data Source=sample.db"))
+        {
+            connection.Open();
+
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "CREATE TABLE IF NOT EXISTS SampleTable (Id INT, Name TEXT)";
+                command.ExecuteNonQuery();
+            }
+
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "INSERT INTO SampleTable (Id, Name) VALUES (1, 'John Doe')";
+                command.ExecuteNonQuery();
+            }
+
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "SELECT * FROM SampleTable";
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"Id: {reader.GetInt32(0)}, Name: {reader.GetString(1)}");
+                    }
+                }
+            }
+        }
+    }
+
 }
