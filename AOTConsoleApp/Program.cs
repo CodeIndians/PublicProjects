780959e0e3c4382eff1e77816e08fd3343f72525
satishCodeIndians <satish@codeindians.com>
2024-01-04 09:30:49 +0530

    Project to test SQLite, NAudio and Mousekeyhook packages

---
+++ AOTConsoleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using NAudio.Wave;
+using Gma.System.MouseKeyHook;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Testing Microsoft.Data.Sqlite, NAudio, and Gma.System.MouseKeyHook");
+
+        // Microsoft.Data.Sqlite
+        using (var connection = new Microsoft.Data.Sqlite.SqliteConnection("Data Source=test.db"))
+        {
+            connection.Open();
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "CREATE TABLE IF NOT EXISTS SampleTable (Id INTEGER PRIMARY KEY, Name TEXT)";
+                command.ExecuteNonQuery();
+            }
+        }
+
+        // NAudio
+        WaveOutEvent waveOut = new WaveOutEvent();
+        AudioFileReader audioFile = new AudioFileReader("test.wav");
+        waveOut.Init(audioFile);
+        waveOut.Play();
+
+        // Gma.System.MouseKeyHook
+        IKeyboardMouseEvents globalHook = Hook.GlobalEvents();
+        globalHook.KeyPress += (sender, e) =>
+        {
+            Console.WriteLine($"Key Pressed: {e.KeyChar}");
+        };
+
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+
+        // Clean up
+        globalHook.Dispose();
+        waveOut.Stop();
+        waveOut.Dispose();
+    }
+}
+
